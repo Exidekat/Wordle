77574005edad9678b8029c9d273b3e8d678fbf98
NealC17 <nealxchandra@gmail.com>
2023-11-16 10:59:27 -0800

    add windows support

---
+++ CMakeFiles/cmpe126project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmpe126project.dir/main.cpp.o"
+  "CMakeFiles/cmpe126project.dir/main.cpp.o.d"
+  "cmpe126project"
+  "cmpe126project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmpe126project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/cmpe126project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmpe126project.
+# This may be replaced when dependencies are built.
