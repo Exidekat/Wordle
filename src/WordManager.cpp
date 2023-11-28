eb8b68b0e2f2779c134e24669a9c71e935479a17
NealC17 <nealxchandra@gmail.com>
2023-11-28 10:59:38 -0800

    Fixed game logic

--- src/WordManager.cpp
+++ src/WordManager.cpp
@@ -25,35 +25,58 @@ void getWords(vector<string> &v, string path){
 
 }
 
+bool checkForRepeats(string word, string guess, char letter, string currentoutput){
+	
+	
+
+	int count = 0;
+	for(int i = 0; i < 5; i++){
+		
+		if(word[i] == letter){
+			count++;
+		}
+
+	}
+
+
+	for(int i = 0; i < 5; i++){
+		if((currentoutput[i] == 'g' || currentoutput[i] == 'y')&& guess[i] == letter) {
+			count--;
+		}
+
+	}
+
+	return count > 0;
+}
+
+
 string gameLogic(string word, string guess){
 	string out = "";
-
 	for(int i = 0; i < 5; i++){
 		
 		if(word[i] == guess[i]) out += "g";
-		else if(word.find(guess[i]) < 5) out += "y";
 		else out += ".";
 
 	}
 
+	for(int i = 0; i < 5; i++){
+		if(out[i] != 'g'&& checkForRepeats(word, guess, guess[i], out)) out[i] = 'y';
+	}
+
 	return out;
-
-
-
 }
-
-map<string, double> getProbs(vector<string> &words, string actual){
+map<string, vector<string>*> getProbs(vector<string> &words, string actual){
 	
-	map<string, double> map;
+	map<string, vector<string>*> map;
 	for(int i  =0; i < words.size(); i++){
 		
-		if(words[i] != actual) map[gameLogic(actual, words[i])] = 0;
+		if(words[i] != actual) map[gameLogic(actual, words[i])] = new vector<string>();
 
 	}
 	
 	for(int i = 0; i < words.size(); i++){
 		
-		if(words[i] != actual) map[gameLogic(actual, words[i])] += 1.0/words.size();
+		if(words[i] != actual) map[gameLogic(actual, words[i])]->push_back(words[i]);
 
 	}
 
@@ -76,7 +99,6 @@ map<string, double> getProbs(vector<string> &words, string actual){
 	
 	//do something here to make the keys correspond to the values;
 	
-		
 
 	return out;
 }
@@ -94,24 +116,35 @@ int main(){
 	
 	
 	
-	//we need to calculate the probability for all words, then we need to calculate p log(p) for all the words
+	//we need to calculate the probability for all words, then we need to calculate p log(1/p) for all the words
 	//whichever value of entropy is highest is the best word. whichever has the entropy at the 25%ile and 75%ile will be used to make the tree
+	// 1. for each word in words, sum entropy(word, other word). whichever word has the highest entropy is the word we select
+	// 2. guess selected word, observe the outcome, suppose it is x = '..gy.'
+	// 3. then in the map, for each word that had an outcome of x, guess a word in that set that had the highest entropy
+	// 	also save words that are in the 5%ile and 95%ile of guesses to make the tree
+	// 4. repeat 2-> 4 until word is found 
 	string actual;
 	cout << "Enter an actual word : " ; 
 	cin >> actual;
-	map<string, double> map = getProbs(words, actual); 
+	//need to sort this based on length of vector<string>*
+	map<string, vector<string>*> map = getProbs(words, actual); 
 		
 
-	double sum = 0;	
+	double sum = 0, p;	
 	for(auto i : map){
 		
-		cout << i.first << "\t" << i.second << endl;
-		sum += i.second* log2(1/i.second);
+		//cout << i.first << "\t" << i.second << endl;
+		p = i.second->size() / static_cast<double>(words.size());
+		sum += p* log2(1/p);
+		
+		cout << i.first << "\t";	
+		for(const auto& element: *i.second) cout << element << " ";
+		cout << endl;	
 	}
 
 
 	cout << endl << endl << sum << endl;
-	
+	//cout << gameLogic("tests", actual) << endl;	
 	
 	return 0;	
 
